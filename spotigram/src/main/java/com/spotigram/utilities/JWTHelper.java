197c43d3594fab8caa8c43acc78a8adc72f382a3
Jose <Joe.Antonio.Perez@gmail.com>
2018-11-28 09:17:15 -0600

    added the log functionality, please update your maven project to use the logger

--- spotigram/src/main/java/com/spotigram/utilities/JWTHelper.java
+++ spotigram/src/main/java/com/spotigram/utilities/JWTHelper.java
@@ -1,16 +1,18 @@
 package com.spotigram.utilities;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.apache.log4j.Logger;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+
 public class JWTHelper {
 	private static Algorithm algo = Algorithm.HMAC512("supasekrit");
+	private static Logger log = Logger.getRootLogger();
 	public static  String generateToken(String username) {
 		LocalDate today = LocalDate.now();
 		LocalDate expiration = LocalDate.of(
@@ -23,14 +25,15 @@ public class JWTHelper {
 				withIssuedAt(java.sql.Date.valueOf(today)).
 				withExpiresAt(java.sql.Date.valueOf(expiration)).
 				sign(algo);
-		System.out.println(token);
-		
+		log.info("Assigning user: " + username + " with token: " + token );
 		return token;
 	}
 	public static Boolean verifyToken(String username, String userToken) {
+		log.info("Verifying user: " + username + " with a jwt token of " + userToken);
 		try {
 			JWTVerifier verifier = JWT.require(algo).withIssuer(username).build();
 			DecodedJWT jwt = verifier.verify(userToken);
+			log.info("User " + username + " was verified successfully");
 			return true;
 		}catch (JWTDecodeException exception){
 			System.out.println("User: "+ username + " tried to do something with an invalid token");
