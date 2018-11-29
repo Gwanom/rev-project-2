2511fca9b50c62f31e1316d79620e3485b3e2669
Josh S <jhstoltenberg@gmail.com>
2018-11-29 09:22:38 -0600

    Add PostMapping to submit new posts
    A new mapping now exists, sending a POST request to /posts will save a
    new post. The body of the request should contain a PostsModel object

--- spotigram/src/main/java/com/spotigram/controllers/PostController.java
+++ spotigram/src/main/java/com/spotigram/controllers/PostController.java
@@ -2,19 +2,19 @@ package com.spotigram.controllers;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spotigram.models.PostsModel;
-import com.spotigram.models.UserModel;
 import com.spotigram.services.PostService;
-import com.spotigram.services.UserServices;
 
 @RestController
 @RequestMapping("posts")
@@ -37,6 +37,13 @@ public class PostController {
 		List<PostsModel> results = postService.findAll();
 		System.out.println(results);
 		return results;
+	}
+	
+	@PostMapping
+	@ResponseBody
+	@ResponseStatus(code=HttpStatus.CREATED)
+	public int savePostsModel(@RequestBody PostsModel pm) {
+		return postService.save(pm);
 	}
 	
 	@GetMapping("/id/{postId}")
