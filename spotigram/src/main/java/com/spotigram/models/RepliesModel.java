fdc98ea8795be0ffe5fdb5e09d0b70685cc458a7
Josh S <jhstoltenberg@gmail.com>
2018-11-28 19:59:01 -0600

    Add spring data annotations to RepliesModel

--- spotigram/src/main/java/com/spotigram/models/RepliesModel.java
+++ spotigram/src/main/java/com/spotigram/models/RepliesModel.java
@@ -1,38 +1,58 @@
 package com.spotigram.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "replies")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class RepliesModel {
-    private int sg_reply_id;
-    private int parent;
-    private int author;
-    private String content;
-    
-    
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "sg_reply_id")
+	private int replyId;
+
+	@ManyToOne
+	@JoinColumn(name = "parent")
+	private PostsModel parent;
+
+	@ManyToOne
+	@JoinColumn(name = "author")
+	private UserModel author;
+
+	private String content;
+
 	public RepliesModel() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-
-	public RepliesModel(int sg_reply_id, int parent, int author, String content) {
+	public RepliesModel(int replyId, PostsModel parent, UserModel author, String content) {
 		super();
-		this.sg_reply_id = sg_reply_id;
+		this.replyId = replyId;
 		this.parent = parent;
 		this.author = author;
 		this.content = content;
 	}
 
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + author;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
 		result = prime * result + ((content == null) ? 0 : content.hashCode());
-		result = prime * result + parent;
-		result = prime * result + sg_reply_id;
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		result = prime * result + replyId;
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -43,25 +63,56 @@ public class RepliesModel {
 		if (getClass() != obj.getClass())
 			return false;
 		RepliesModel other = (RepliesModel) obj;
-		if (author != other.author)
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
 			return false;
 		if (content == null) {
 			if (other.content != null)
 				return false;
 		} else if (!content.equals(other.content))
 			return false;
-		if (parent != other.parent)
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
 			return false;
-		if (sg_reply_id != other.sg_reply_id)
+		if (replyId != other.replyId)
 			return false;
 		return true;
 	}
 
+	public int getReplyId() {
+		return replyId;
+	}
 
-	@Override
-	public String toString() {
-		return "RepliesModel [sg_reply_id=" + sg_reply_id + ", parent=" + parent + ", author=" + author + ", content="
-				+ content + "]";
+	public void setReplyId(int replyId) {
+		this.replyId = replyId;
+	}
+
+	public PostsModel getParent() {
+		return parent;
+	}
+
+	public void setParent(PostsModel parent) {
+		this.parent = parent;
+	}
+
+	public UserModel getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(UserModel author) {
+		this.author = author;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
 	}
 
 }
